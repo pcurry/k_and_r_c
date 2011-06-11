f28e931b10820e7317d0020a6b74e64e3830dc9b
Paul Curry <pcurry@gmail.com>
2011-06-10 23:39:38 -0400

    Exercise 1-6

--- chapter_1/input_to_output.c
+++ chapter_1/input_to_output.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
-/* copy input to output; 2nd version */
+void test_c(int c);
+
+/* copy input to output; 3rd version, exercise 1-6 */
 main() {
   int c;
 
-  while ((c = getchar()) != EOF) {
-    putchar(c);
+  while (c = getchar() != EOF) {   
+    printf("Result of getchar() != EOF: %d\n", c);
+    test_c(c);
+    printf("\n\n");    
+  }
+  printf("After the while loop is broken, the variable c still has a value: %d\n", c);
+  test_c(c);
+  printf("\n\n");     
+}
+
+void test_c(int c) {
+  if(c == 1 || c == 0) {
+    printf("c is either 1 or 0");
   }
 }
