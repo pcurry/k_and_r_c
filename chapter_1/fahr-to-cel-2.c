98493fa8165f6844fe2cda4103497e64ee2c7bb5
Paul Curry <pcurry@gmail.com>
2011-06-10 23:12:30 -0400

    Reversed chart for Exercise 1-5

--- chapter_1/fahr-to-cel-2.c
+++ chapter_1/fahr-to-cel-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define  LOWER  0      /* lower limit of temperature table */
-#define  UPPER  300  /* upper limit */
+#define  UPPER  300   /* upper limit */
 #define  STEP   20    /* step size */
 
 /* print Fahrenheit-Celsius table 
@@ -14,7 +14,8 @@ main() {
     
   printf("Fahr\tCelsius\n");
 
-  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) { 
+  /* Exercise 1-5 - reverse the list */
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) { 
     printf("%3d \t%6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
   }
 }
