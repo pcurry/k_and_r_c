39b20b3d008c23135d3070d3c1058747e3e7faaf
Paul Curry <pcurry@gmail.com>
2011-06-20 21:36:58 -0400

    print each word on its own line

---
+++ chapter_1/word_per_line.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define IN  1  /* inside a word */
+#define OUT 0  /* outside a word */
+
+#define PRESERVE_NONWORD 1 /* preserve non-word characters */
+
+/* Break my input into words, and print out one per line. */
+main() {
+  int c, state;
+  
+  state = OUT;
+  while((c = getchar()) != EOF) {
+
+    if (c == ' ' || c == '\n' || c == '\t') {
+      if (state == IN)
+	putchar('\n');
+      state = OUT;
+      if(PRESERVE_NONWORD)
+	putchar(c);
+    }
+    else if (state == OUT) {
+      state = IN;
+      putchar(c);
+    }
+  }
+}
+      
