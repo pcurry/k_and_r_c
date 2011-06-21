1e2bbc94f41fb5e03ce6084ddf78c43f2c8a6983
Paul Curry <pcurry@gmail.com>
2011-06-21 11:05:22 -0400

    word_per_line.c: don't save whitespace

--- chapter_1/word_per_line.c
+++ chapter_1/word_per_line.c
@@ -3,7 +3,7 @@
 #define IN  1  /* inside a word */
 #define OUT 0  /* outside a word */
 
-#define PRESERVE_NONWORD 1 /* preserve non-word characters */
+#define PRESERVE_NONWORD 0 /* preserve non-word characters */
 
 /* Break my input into words, and print out one per line. */
 main() {
@@ -19,8 +19,9 @@ main() {
       if(PRESERVE_NONWORD)
 	putchar(c);
     }
-    else if (state == OUT) {
-      state = IN;
+    else {
+      if (state == OUT) 
+	state = IN;
       putchar(c);
     }
   }
